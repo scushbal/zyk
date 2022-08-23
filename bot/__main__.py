00ccb145566bf5015fadbc07b69e195c98854386
Sahrul Maulana <111454488+sahrulmaulana@users.noreply.github.com>
2022-08-24 06:47:06 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -29,6 +29,7 @@ def stats(update, context):
     else:
         last_commit = 'No UPSTREAM_REPO'
     currentTime = get_readable_time(time() - botStartTime)
+    current = now.strftime('%m/%d %I:%M:%S %p')
     osUptime = get_readable_time(time() - boot_time())
     total, used, free, disk= disk_usage('/')
     total = get_readable_file_size(total)
