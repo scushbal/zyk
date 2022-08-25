b7871ab8639c91aadc583c18d8d2e0e47821708b
sahrulmaulana <ryjprjkt@gmail.com>
2022-08-25 23:22:29 +0700

    [Redeploy Required] changed base - Major Improvements

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/sahrulmaulana/mirror-bot-tg"
+    UPSTREAM_REPO = "https://github.com/arshsisodiya/helios-mirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email ryjprjkt@gmail.com \
-                 && git config --global user.name sahrulmaulana \
+                 && git config --global user.email arshtwitterbot@gmail.com \
+                 && git config --global user.name helios \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
